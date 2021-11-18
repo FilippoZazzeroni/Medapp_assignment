ee6d17f915c2164a290c52f60472bd5606415186
Filippo Zazzeroni <filippo.zazzeroni@gmail.com>
2021-11-18 12:36:03 +0100

    Starting commit

---
+++ android/app/src/main/kotlin/com/example/medapp_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medapp_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/color_palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const primary = Color.fromRGBO(82, 135, 143, 1.0);
+
+  static const secondary = Color.fromRGBO(242, 242, 242, 1.0);
+
+  static const background = Color.fromRGBO(230, 230, 230, 1.0);
+
+  static const mantis = Color.fromRGBO(144, 190, 81, 1.0);
+}
